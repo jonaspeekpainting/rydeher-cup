23c42ae5e2316a6bd9ad8eac439d85c0a567d6ae
jonaspeekpainting <jonas@rlpeekpainting.com>
2026-03-28 12:29:33 -0700

    Initial Commit

---
+++ ryderher-cup/ryderher_cupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ryderher_cupApp.swift
+//  ryderher-cup
+//
+//  Created by Jonas Peek on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct ryderher_cupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ryderher-cupTests/ryderher_cupTests.swift
@@ -0,0 +1,17 @@
+//
+//  ryderher_cupTests.swift
+//  ryderher-cupTests
+//
+//  Created by Jonas Peek on 3/28/26.
+//
+
+import Testing
+@testable import ryderher_cup
+
+struct ryderher_cupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
